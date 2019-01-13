c758635319593908fd87b310a46baae132c625c4
muralee <kananhere@gmail.com>
2019-01-13 14:17:27 +0800

    rest api sample

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("welcome to nde-restapi");
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('request received from')
+});
+
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3]);
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
